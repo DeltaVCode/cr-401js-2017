59a45e9d105b0e16337df61e735222f64c12d181
Keith Dahlby <keith@newbo.co>
2017-08-17 08:59:24 -0500

    Handle non-palette bitmaps

--- 04-event_emitter_and_bitmap_transformer/demo/bitmap-reader/bitmap-reader.js
+++ 04-event_emitter_and_bitmap_transformer/demo/bitmap-reader/bitmap-reader.js
@@ -10,11 +10,12 @@ const bmp = {};
 bmp.type = bitmap.toString('utf-8', 0, 2);
 bmp.size = bitmap.readUInt32LE(2);
 bmp.offset = bitmap.readUIntLE(0x0A, 4);
+bmp.headerSize = bitmap.readUIntLE(0x0E, 4);
 bmp.width = bitmap.readUIntLE(0x12, 4);
 bmp.height = bitmap.readUIntLE(0x16, 4);
 
 bmp.bpp = bitmap.readUIntLE(0x1C, 2);
-bmp.paletteColorCount = bitmap.readUIntLE(0x2E, 4) || (1 << bmp.bpp);
+bmp.paletteColorCount = bitmap.readUIntLE(0x2E, 4) || (bmp.bpp > 8 ? 0 : 1 << bmp.bpp);
 
 console.log('bitmap:', bmp);
 
@@ -23,7 +24,10 @@ const img = bitmap.slice(bmp.offset);
 img.fill(0x1c, 0, bmp.width * 10);
 
 const bytesPerColor = 4;
-const palette = bitmap.slice(0x36, bmp.paletteColorCount * bytesPerColor);
+const paletteOffset = 0x36;
+const paletteEndOffset = Math.min(paletteOffset + bmp.paletteColorCount * bytesPerColor, bmp.offset);
+const palette = bitmap.slice(paletteOffset, paletteEndOffset);
+console.log(`palette length: ${palette.length}`)
 
 // Standard 16 colors
 const colors = [
@@ -44,7 +48,9 @@ const colors = [
   0x00ffff,
   0xffffff,
 ];
-colors.forEach((c, i) => palette.writeUIntLE(c, i << 2, 3));
+if (palette.length >= bytesPerColor * colors.length) {
+  colors.forEach((c, i) => palette.writeUIntLE(c, i << 2, 3));
+}
 
 console.log('palette', palette.toString('hex', 0));
 
