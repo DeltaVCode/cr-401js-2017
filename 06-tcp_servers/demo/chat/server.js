2d375bd0a187e0176b935a545edd3de6779884e6
Keith Dahlby <keith@newbo.co>
2017-08-21 11:31:20 -0500

    Add client ID and event emitter

--- 06-tcp_servers/demo/chat/server.js
+++ 06-tcp_servers/demo/chat/server.js
@@ -6,19 +6,36 @@ const server = net.createServer();
 
 const pool = [];
 
-server.on('connection', function(socket) {
-  pool.push({
-    socket
+const EE = require('events');
+const ee = new EE();
+
+ee.on('@all', function (sender, message) {
+  pool.forEach(receiver => {
+    if (receiver.id === sender.id) return;
+
+    console.log(`Client ${receiver.id} receiving message`)
+    receiver.socket.write(`${sender.id}: ${message}`);
   });
+})
+
+let id = 1;
+server.on('connection', function(socket) {
+  const client = {
+    id: id++,
+    socket
+  };
+  socket.write(`Welcome. Your ID is ${client.id}.\r\n`);
+  pool.push(client);
 
   socket.on('data', function (data) {
     console.log(data);
 
-    pool.forEach(client => {
-      client.socket.write(`echo: ${data.toString()}`)
-    })
+    ee.emit('@all', client, data.toString());
   });
 
+  socket.on('error', function (err) {
+    console.warn(err);
+  })
 });
 
 server.listen(PORT, function() {
