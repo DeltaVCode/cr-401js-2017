aabce5c24add66d9c5c2d25dad576ec295447dec
Keith Dahlby <keith@newbo.co>
2017-08-24 12:39:35 -0500

    Add basic (sync!) file storage

--- 09-vanilla_api_persistence/demo/vanilla-rest-api-persistence/lib/storage.js
+++ 09-vanilla_api_persistence/demo/vanilla-rest-api-persistence/lib/storage.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const storage = {};
+const path = require('path');
+const fs = require('fs');
 
 module.exports = exports = {};
 
 exports.createItem = function(schemaName, item) {
   if (!schemaName) return Promise.reject(new Error('expected schemaName'));
   if (!item) return Promise.reject(new Error('expected item'));
-  if (!storage[schemaName]) storage[schemaName] = {};
 
-  storage[schemaName][item.id] = item;
+  const filePath = `${__dirname}/../data/${schemaName}/${item.id}.json`;
+  ensureDirectoryExistence(filePath);
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(item));
 
   return Promise.resolve(item);
 };
@@ -19,12 +23,25 @@ exports.fetchItem = function(schemaName, id) {
     if (!schemaName) return reject(new Error('expected schema name'));
     if (!id) return reject(new Error('expected id'));
 
-    var schema = storage[schemaName];
-    if (!schema) return reject(new Error('schema not found'));
+    const filePath = `${__dirname}/../data/${schemaName}/${id}.json`;
+    if (!fs.existsSync(path.dirname(filePath)))
+      return reject(new Error('schema not found'));
 
-    var item = schema[id];
-    if (!item) return reject(new Error('item not found'));
+    // TODO: reject(new Error('item not found'));
 
+    var data = fs.readFileSync(filePath);
+    // TODO: catch/reject parsing error
+    var item = JSON.parse(data.toString());
     resolve(item);
   });
 };
+
+// https://stackoverflow.com/a/34509653
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
---
+++ 09-vanilla_api_persistence/demo/vanilla-rest-api-persistence/test/storage.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { expect } = require('chai');
+const storage = require('../lib/storage');
+
+describe.only('storage', function() {
+  const schemaName = 'people';
+  const itemToSave = { id: 12, name: 'Keith' };
+
+  describe('createItem', function() {
+    it('should save item', function(done) {
+      storage.createItem(schemaName, itemToSave)
+        .then(saveItem => {
+          expect(saveItem).to.deep.equal(itemToSave);
+          done();
+        })
+        .catch(done)
+    });
+  });
+
+  describe('fetchItem', function() {
+    it('should fetch item', function (done) {
+      storage.fetchItem(schemaName, itemToSave.id)
+        .then(fetchedItem => {
+          expect(fetchedItem).to.deep.equal(itemToSave);
+          done();
+        })
+        .catch(done)
+    });
+
+      it('should fail given missing schema', function (done) {
+        storage.fetchItem('missing', itemToSave.id)
+          .catch(err => {
+            expect(err).to.not.be.null
+            done();
+          });
+      });
+  });
+});
