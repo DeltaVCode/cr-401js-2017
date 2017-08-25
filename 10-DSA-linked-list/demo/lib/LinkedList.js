5469c32dbe87c0c9eda9d2e916ec32a3be034bb3
Keith Dahlby <keith@newbo.co>
2017-08-25 13:50:05 -0500

    Add LinkedList

---
+++ 10-DSA-linked-list/demo/lib/LinkedList.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const LinkedList = module.exports = function () {
+  this.head = null;
+  this.length = 0;
+};
+
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+LinkedList.prototype.toArray = function() {
+  const result = [];
+
+  let node = this.head;
+  while (node !== null) {
+    result.push(node.value);
+    node = node.next;
+  }
+
+  /*
+  for (let node = this.head; node !== null; node = node.next) {
+    result.push(node.value);
+  }
+  */
+  return result;
+}
+
+LinkedList.prototype.count = function() {
+  var count = 0;
+  let node = this.head;
+  while (node !== null) {
+    count++;
+    node = node.next;
+  }
+  return count;
+}
+
+LinkedList.prototype.prepend = function(value) {
+  const node = new Node(value);
+  node.next = this.head;
+  this.head = node;
+
+  this.length += 1;
+};
+
+LinkedList.prototype.append = function(value) {
+  const newNode = new Node(value);
+  newNode.next = null;
+
+  this.length++;
+
+  let node = this.head;
+  if (node === null) {
+    this.head = newNode;
+    return;
+  }
+  while (node.next !== null) {
+    node = node.next;
+  }
+  node.next = newNode;
+}
+
+LinkedList.prototype.insert = function(index, value) {
+  if (index === 0)
+    return this.prepend(value);
+
+  this.length++;
+
+  let node = this.head;
+  for (let i = 1; i < index; i++) {
+    node = node.next;
+  }
+  const newNode = new Node(value);
+  newNode.next = node.next;
+  node.next = newNode;
+}
---
+++ 10-DSA-linked-list/demo/test/ll.js
@@ -0,0 +1,102 @@
+const assert = require('assert');
+
+const LL = require('../lib/LinkedList');
+
+describe('LinkedList', function() {
+  describe('count', function() {
+    it ('should be zero for new list', function() {
+      var ll = new LL();
+
+      var count = ll.count();
+
+      assert.equal(count, 0);
+      assert.equal(ll.length, 0);
+    })
+    it ('should be two for list with two things', function() {
+      var ll = new LL();
+      ll.prepend(17);
+      ll.prepend(Math.PI);
+
+      var count = ll.count();
+
+      assert.equal(count, 2);
+      assert.equal(ll.length, 2);
+    })
+  })
+  describe('prepend', function() {
+    it ('should insert value at beginning', function() {
+      // Arrange
+      var ll = new LL();
+
+      // Act
+      ll.prepend(1);
+      assert.equal(ll.head.value, 1);
+      assert.deepEqual(ll.toArray(), [1]);
+      assert.equal(ll.length, 1);
+
+      ll.prepend(2);
+      assert.equal(ll.head.value, 2);
+      assert.deepEqual(ll.toArray(), [2, 1]);
+      assert.equal(ll.length, 2);
+    });
+  });
+  describe('append', function() {
+    it('should insert value at end', function() {
+      var ll = new LL();
+
+      var first = 7;
+      ll.append(first);
+      assert.equal(ll.head.value, first);
+      assert.deepEqual(ll.toArray(), [first]);
+      assert.equal(ll.length, 1);
+
+      var second = '42';
+      ll.append(second);
+      assert.equal(ll.head.value, first);
+      assert.deepEqual(ll.toArray(), [first, second]);
+      assert.equal(ll.length, 2);
+    })
+  })
+  describe('insert', function() {
+    it('should work at the beginning', function() {
+      var ll = new LL();
+
+      ll.insert(0, 15);
+      assert.deepEqual(ll.toArray(), [15]);
+
+      ll.insert(0, 75);
+      assert.deepEqual(ll.toArray(), [75, 15]);
+    });
+    it('should work at the end', function() {
+      var ll = new LL();
+
+      ll.insert(0, 89);
+      ll.insert(1, 42);
+      ll.insert(2, 'katz');
+      assert.deepEqual(ll.toArray(), [89, 42, 'katz']);
+    })
+    it('should works in the middle', function() {
+      var ll = new LL();
+
+      ll.insert(0, 1);
+      assert.deepEqual(ll.toArray(), [1]);
+      assert.equal(ll.length, 1);
+
+      ll.insert(1, 2);
+      assert.deepEqual(ll.toArray(), [1, 2]);
+      assert.equal(ll.length, 2);
+
+      ll.insert(1, 3);
+      assert.deepEqual(ll.toArray(), [1, 3, 2]);
+      assert.equal(ll.length, 3);
+
+      ll.insert(0, 4);
+      assert.deepEqual(ll.toArray(), [4, 1, 3, 2]);
+      assert.equal(ll.length, 4);
+
+      ll.insert(1, 5);
+      assert.deepEqual(ll.toArray(), [4, 5, 1, 3, 2]);
+      assert.equal(ll.length, 5);
+    })
+  })
+});
