1fef33efd883ff456216e9b8549af5191251d98f
Keith Dahlby <keith@newbo.co>
2017-10-10 12:27:22 -0500

    Add signup/signin reducer and action creators

--- 32-auth/demo/frontend/src/component/landing-container/index.js
+++ 32-auth/demo/frontend/src/component/landing-container/index.js
@@ -3,14 +3,15 @@ import { connect } from 'react-redux';
 
 import AuthForm from '../auth-form';
 import * as util from '../../lib/util';
+import { signupRequest, signinRequest } from '../../action/auth-actions';
 
 class LandingContainer extends React.Component {
   render() {
     const { params } = this.props.match;
 
     let handleComplete = params.auth === 'login' ?
-      user => console.log('Login', user) :
-      user => console.log('Sign Up', user);
+      this.props.signin :
+      this.props.signup;
 
     return (
       <div>
@@ -22,4 +23,9 @@ class LandingContainer extends React.Component {
   }
 }
 
-export default LandingContainer;
+let mapStateToProps = undefined;
+let mapDispatchToProps = (dispatch) => ({
+  signup: (user) => dispatch(signupRequest(user)),
+  signin: (user) => dispatch(signinRequest(user)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(LandingContainer);
--- 32-auth/demo/frontend/src/lib/app-create-store.js
+++ 32-auth/demo/frontend/src/lib/app-create-store.js
@@ -1,9 +1,11 @@
 import reducer from '../reducer';
 import thunk from './redux-thunk.js';
 import reporter from './redux-reporter.js';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let appStoreCreate = () =>
-  createStore(reducer, applyMiddleware(thunk, reporter))
+  createStore(reducer,
+     composeEnhancers(applyMiddleware(thunk, reporter)));
 
 export default appStoreCreate;
