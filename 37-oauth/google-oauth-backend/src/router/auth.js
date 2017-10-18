b9be4005f82ee11716578029ce6ad6f1c9e1e224
Keith Dahlby <keith@newbo.co>
2017-10-18 14:23:47 -0500

    Move Google authorize to redirect

--- 37-oauth/google-oauth-backend/src/router/auth.js
+++ 37-oauth/google-oauth-backend/src/router/auth.js
@@ -10,6 +10,18 @@ function redirectToClient(res, reason) {
   res.redirect(`${process.env.CLIENT_URL}${process.env.CLIENT_URL.indexOf('?') < 0 ? '?' : '&'}reason=${reason}`);
 }
 export default new Router()
+.get('/login/google', (req, res, next) => {
+  let AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+  let clientId = `client_id=${process.env.GOOGLE_CLIENT_ID}`
+  let oauthStuff = 'response_type=code&scope=openid%20profile%20email'
+  let redirectUri = `redirect_uri=${process.env.CLIENT_URL}/oauth/google/code`;
+
+  let actualUrl =
+    `${AUTH_URL}?${clientId}&${oauthStuff}&${redirectUri}`;
+  console.log(actualUrl);
+
+  res.redirect(actualUrl);
+})
 .get('/oauth/google/code', (req, res, next) => {
   if (!req.query.code) {
     redirectToClient(res, 'no-code');
